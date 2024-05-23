ad775e007fc4cb56132d99f9f5dcfe809aa464e1
leeyejun1215 <happytok2@naver.com>
2024-05-23 18:15:39 +0900

    Create 11주차.py

---
+++ 11주차.py
@@ -0,0 +1,14 @@
+def validate_resident_registration_number(a):
+    if "-" in a:
+        a.remove("-")
+    valid_check_code = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5]
+    total = 0
+    for k in range(0, 12):
+        total += int(a[k])*valid_check_code[k]
+    if (11-(total % 11)) % 10 == int(a[-1]):
+        return "주민등록번호가 유효합니다."
+    else:
+        return "주민등록번호가 유효하지 않습니다."
+
+resident_registration_number = list(input("주민등록번호를 입력하세요: "))
+print(validate_resident_registration_number(resident_registration_number))
